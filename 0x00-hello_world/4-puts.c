c199d23b81cd1ef9c8212a2463e3f2473aae6a27
mohammedfereje <mfereje104@gmail.com>
2022-06-30 04:55:39 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * description: prints "programming is like building multilingual puzzle"
+ * retun: 0
+ * */
+int main(void){
+puts("\"Programming is like building multilingual puzzle");
+return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * description:prints "with proper grammer, but the outcome is a piece of art,"
+ * return:0
+ * */
+int main(void){
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return 0;
+}
