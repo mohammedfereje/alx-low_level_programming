08d20758aa71a377e3c91b2ef466f4095852b5e9
mohammedfereje <mfereje104@gmail.com>
2022-07-04 05:40:26 -0700

    C-variable and conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 /*  more headers goes there*/
- /* main -entry point */
+ /**
+  *  main -entry point
+  *  return: 0 means no error 
+  */
 /* betty style doc for function main goes there */
 int main(void)
 {
