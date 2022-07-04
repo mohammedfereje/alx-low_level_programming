711bd68262f62843dbee47327802ff2b37cba2b0
mohammedfereje <mfereje104@gmail.com>
2022-07-04 04:37:22 -0700

    C-variable and conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,19 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void){
+int main(void)
+{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
+	if (n > 0) 
 		printf("%d is posetive \n", n);
-	
-	}
-	else if( n==0){
+	else if (n == 0) {
 		printf("%d is zero \n", n);
 	}
-	else
-	{
+	else {
 		printf("%d is negative \n", n);
-		
 		}	
 
 	return (0);
