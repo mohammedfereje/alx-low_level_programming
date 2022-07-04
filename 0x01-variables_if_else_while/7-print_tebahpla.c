53528b55763391ddaca068096a2f4ceb71678c83
mohammedfereje <mfereje104@gmail.com>
2022-07-04 06:48:55 -0700

    reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/*main()-entry point*/
 /* betty style doc for function main goes there */
-int main(void){
+int main(void)
+{
 	int x = 'z';
-	
-	for( ;x >='a'; x--){
-    
+	for( ;x >='a'; x--)
         putchar(x);
-    }
-    
      putchar('\n');
 	return (0);
 }
