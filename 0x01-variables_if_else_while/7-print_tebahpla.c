1efc42a30d694b4b0cc241021e93f4bf2371fb88
mohammedfereje <mfereje104@gmail.com>
2022-07-04 03:17:56 -0700

    C-Variables-if-else-while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,13 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
-{
+int main(void){
 	int x = 'z';
 	
-
-	for( x;x >='a'; x--)
-    {
+	for( ;x >='a'; x--){
     
         putchar(x);
     }
