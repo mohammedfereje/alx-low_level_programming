1cd94274b2e9d4f0bc9db7b5ae00ebd0254ae9fe
mohammedfereje <mfereje104@gmail.com>
2022-07-05 06:32:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "main.h"
 
-int main (void)
+int main(void)
 {
-char ar[10]="_putchar";
-int i=0;
-while(ar[i]!='\0')
+char ar[10] = "_putchar";
+int i = 0;
+while(ar[i] != '\0')
 {
 putchar(ar[i]);
 i++;
 }
 putchar('\n');
-return(0);
+return (0);
 }
