a9b18f7dc77f4c4df6ba28f04268d165f26ebe0d
mohammedfereje <mfereje104@gmail.com>
2022-07-15 00:33:50 -0700

    c-hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * main -entry point
  * description: prints "programming is like building multilingual puzzle"
+ *
  * Return :returns 0
- */
+ **/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
